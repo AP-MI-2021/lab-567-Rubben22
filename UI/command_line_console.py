ff21814014e8a83cdc184f55b4ba37cdd47c1ec9
[Rubben22] <pasca.ruben120@gmail.com>
2021-11-04 13:17:28 +0200

    consola_noua

--- UI/command_line_console.py
+++ UI/command_line_console.py
@@ -10,39 +10,48 @@ def meniu():
     print("add, id, nume, clasa, pret, checkin")
     print("delete, id")
     print("showall")
-    print("vezi comenzile")
+    print("iesire")
 
 
-def command(lista):
-    lista = []
+def command(listaNoua):
     while True:
         optiune = input("Dati comenzile: ")
-        if optiune == "vezi comenzile":
+        if optiune == "ajutor":
             meniu()
         else:
             cuvinte = optiune.split(";")
             if cuvinte[0] == "iesire":
                 break
             else:
+                print(cuvinte)
                 for rezervare in cuvinte:
                     cuvant = rezervare.split(",")
-                    if cuvant[0] =="add":
+                    if cuvant[0] == "add":
                         try:
-                            lista = adaugaRezervare(cuvant[1], cuvant[2],cuvant[3],cuvant[4], lista)
+                            id = cuvant[1]
+                            nume = cuvant[2]
+                            clasa = cuvant[3]
+                            pret = float(cuvant[4])
+                            checkin = cuvant[5]
+                            listaNoua = adaugaRezervare(id, nume, clasa, pret, checkin, listaNoua)
                         except ValueError as ve:
                             print("Eroare: {}".format(ve))
-                            return lista
-                    elif cuvant[0] == "delete":
-                        lista = stergeRezervare(cuvant[0], lista)
-                    elif cuvant[0] == "showall":
-                        showAll(lista)
+                            return listaNoua
+                    elif cuvant[0] == 'delete':
+                        try:
+                            id = cuvant[1]
+                            listaNoua= stergeRezervare(id, listaNoua)
+                        except ValueError as ve:
+                            print("Eroare: {}".format(ve))
+                            return listaNoua
+                    elif cuvant[0] == 'showall':
+                        showAll(listaNoua)
                     else:
-                        print("Comanda gresita! Reincercati")
+                        print("Comanda gresita! Reincercati sau tastati 'ajutor' pentru a vedea comenzile")
 
 
 def run_menu():
     lista = []
-    lista = adaugaRezervare("2", "Andrei", "economy plus", 180, "da", lista)
     command(lista)
 
 run_menu()
